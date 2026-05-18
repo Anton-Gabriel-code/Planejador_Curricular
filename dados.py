e26e6767611d3b34b1c91d83b48f93b76c9d3922
Anton-Gabriel-code <antoniothecode@gmail.com>
2026-05-17 22:47:26 -0300

    Disciplinas

---
+++ dados.py
@@ -0,0 +1,71 @@
+DISCIPLINAS: dict[ str, list[str]] = {
+    # 1° Período
+
+    "Fundamentos de Matemática Computacional": [],
+    "Fundamentos da Programação": [],
+    "Fundamentos de P.C.E.A de Comptadores": [],
+    "Inglês para Computação": [],
+
+    # 2° Período
+
+    "Cálculo I": ["Fundamentos de Matemática Computacional"],
+    "Laboratório de Programação": ["Fundamentos da Programação"],
+    "Experiência Extensionista I": [],
+    "Redes de Computadores": ["Fundamentos de P.C.E.A de Computadores"],
+
+    # 3° Período
+
+    "Estatística Computacional": ["Cálculo I"],
+    "Metodologia Cientifíca": [],
+    "Sistemas Digitais": ["Fundamentos de P.C.E.A de Computadores"],
+    "Cálculo II": ["Cálculo I"],
+    "Projeto de Programação": ["Laboratório de Programação"],
+    
+    # 4° Período
+
+    "Engenharia de Software": ["Projeto de Programação"],
+    "Estrutura de Dados": ["Laboratório de Programação"],
+    "Teoria dos Grafos": ["Estatística Computacional"],
+    "Cloud Computing": ["Redes de Computadores"],
+    "Laboratorio de Redes de Computadores": ["Redes de Computadores"],
+
+    # 5° Período
+
+    "Estrutura de Dados Avançada": ["Estrutura de Dados"],
+    "Laboratório de Análise de Algorítmo": ["Estrutura de Dados"],
+    "Laboratorio de Engenharia de Software": ["Engenharia de Software"],
+    "Modelagem de Dados": ["Estrutura de Dados"],
+    "Teoria da Computação": ["Teoria dos Grafos"],
+    
+    #6° Período
+
+    "Experiência Extensionista II": ["Experiência Extensionista I"],
+    "Inteligência Artificial": ["Estrutura de Dados Avançada"],
+    "Linguagens Formais e Autômatos": ["Teoria da Computação"],
+    "Sistemas Operacionais": ["Estrutura de Dados", "Fubdamentos de P.C.E.A de Computadores"],
+
+    # 7° Período
+
+    "Compiladores": ["Linguagens Formais e Autômatos"],
+    "Experiência Extensionista III": ["Experienência Extensionista II"],
+    "Interface Humano Computador": ["Engenharia de Software"],
+    "Laboratório de Banco de Dados": ["Modelagem de Dados"],
+    "Machine Learning": ["INteligência Artificial", "EStatística Computacional"],
+
+    # 8° Período
+
+    "Experiência Extensionista IV": ["Experiência Extensionista III"],
+    "Processamento de Imagens de C Gráfica": ["Cálculo II", "Estrtura de Dados Avançada"],
+    "Programação Avançada": ["EStrutura de Dados Avançada"],
+    "Project Lab": ["Laboratório de ENgenharia de Software", "Laboratório de Banco de Dados"],
+
+    # 9° Período
+
+    "Direito Digital": [],
+    "Estágio Supervisionado": ["Project Lab"],
+    "Experiência Extensionista V": ["Experiência Extensionista IV"],
+    "TCC": ["Compiladores", "Redes de Computadores", "Engenharia de Software", "Laboratório de Banco de Dados"]
+
+
+
+}
